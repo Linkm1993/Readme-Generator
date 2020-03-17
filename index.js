d98e286078e2d18ec489905e49a86752101e3832
Matt Link <Linkm1993@gmail.com>
2020-03-17 15:09:40 -0400

    Installed inquirer.js and chalk-pipe.js, got a test question working

---
+++ index.js
@@ -0,0 +1,15 @@
+//Requirments for node modules
+let fs = require('fs');
+let inquirer = require('inquirer');
+let chalkPipe = require('chalk-pipe');
+
+var questions = [
+    {
+      type: 'input',
+      name: 'first_name',
+      message: "What's your first name"
+    },]
+
+inquirer.prompt(questions).then(answers => {
+    console.log(JSON.stringify(answers, null, '  '));
+      });
